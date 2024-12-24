1ba7b8bab8a0b957a7c471b7295f44428823d142
Shrikant Vithoba Dalvi <157231725+shrikant-d5456@users.noreply.github.com>
2024-12-24 10:25:36 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { BsFillHouseFill, BsFillPersonFill, BsThreeDotsVertical, BsX } from 'react-icons/bs'
 
 const Navbar = () => {

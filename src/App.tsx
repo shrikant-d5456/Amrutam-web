efc0856198541112e867a35b4684dc689ce938c3
Shrikant Vithoba Dalvi <157231725+shrikant-d5456@users.noreply.github.com>
2024-12-24 10:49:14 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,47 @@
-// import React from 'react'
-import Approach from './components/Approach'
-import Consultant from './components/Consultant'
-import DiscoverAayurveda from './components/DiscoverAayurveda'
-import Experts from './components/Experts'
-import Contact from './components/Contact'
-import HeroSection from './components/HeroSection'
-import Navbar from './components/Navbar'
-import PlayStoreApp from './components/PlayStoreApp'
-import Stories from './components/Stories'
+import React, { useEffect, useState } from 'react';
+import Approach from './components/Approach';
+import Consultant from './components/Consultant';
+import DiscoverAayurveda from './components/DiscoverAayurveda';
+import Experts from './components/Experts';
+import Contact from './components/Contact';
+import HeroSection from './components/HeroSection';
+import Navbar from './components/Navbar';
+import PlayStoreApp from './components/PlayStoreApp';
+import Stories from './components/Stories';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer); // Clean up the timer
+  }, []);
+
   return (
     <>
-    <Navbar/>
-    <HeroSection/>
-    <DiscoverAayurveda/>
-    <Consultant/>
-    <Approach/>
-    <Stories/>
-    <Experts/>
-    <PlayStoreApp/>
-    <Contact/>
+      {isLoading ? (
+        <div className='w-full h-screen flex justify-center items-center m-auto '>
+         <div className='border-4 border-b-0 border-green-500 animate-spin rounded-full w-20 h-20 self-center '>
+         </div>
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <HeroSection />
+          <DiscoverAayurveda />
+          <Consultant />
+          <Approach />
+          <Stories />
+          <Experts />
+          <PlayStoreApp />
+          <Contact />
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;

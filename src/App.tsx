3c3dd878b56faf1b31917748fcdb13b6a053dcbe
Shrikant Vithoba Dalvi <157231725+shrikant-d5456@users.noreply.github.com>
2024-12-24 10:58:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Approach from './components/Approach';
 import Consultant from './components/Consultant';
 import DiscoverAayurveda from './components/DiscoverAayurveda';
